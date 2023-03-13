7928872ea76797ebb9e88d116b7dce53ea949ba9
ASCON <ural@ascon.ru>
2023-03-13 10:20:59 +0500

    First

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 import numpy as np
 import cv2
 from matplotlib import pyplot as plt
+img = cv2.imread('TestImg.jpg', cv2.IMREAD_COLOR)
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+plt.imshow(img)
+plt.show()
+
+img.dtype, img.ndim, img.shape, img.size
