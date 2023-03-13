93be4e346c980753109ed8b8bcd2510b8c0d6ae7
ASCON <ural@ascon.ru>
2023-03-13 11:33:57 +0500

    task2

--- main.py
+++ main.py
@@ -3,7 +3,15 @@ import cv2
 from matplotlib import pyplot as plt
 img = cv2.imread('TestImg.jpg', cv2.IMREAD_COLOR)
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-plt.imshow(img)
+# plt.imshow(img)
+# plt.show()
+#
+# img.dtype, img.ndim, img.shape, img.size
+img_normed = img / 127.5 - 1.0
+fig, ax = plt.subplots(1, 2)
+ax[0].imshow(img)
+ax[1].imshow(img_normed)
 plt.show()
 
-img.dtype, img.ndim, img.shape, img.size
+print("Original image:", img.mean(), img.std())
+print("Normed image:", img_normed.mean(), img_normed.std())
